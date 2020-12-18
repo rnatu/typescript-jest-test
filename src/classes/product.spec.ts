13a95e278fdd37effb23c16978ed8ba6ffd5000f
Renato Xavier <rnatu91@gmail.com>
2020-12-18 11:03:28 -0300

    TypeScript - Testes com Jest - Testando Product.ts
    Aula utilizando jest para execução de teste na classe Product.ts

---
+++ src/classes/product.spec.ts
@@ -0,0 +1,17 @@
+import { Product } from './product';
+
+const createSut = (name: string, price: number): Product => {
+  return new Product(name, price);
+};
+
+describe('Product', () => {
+  //limpar os mocks do jest depois de cada teste
+  afterEach(() => jest.clearAllMocks());
+
+  it('should return undefined', () => {
+    // System Under Test -> sut
+    const sut = createSut('Camiseta', 49.9);
+    expect(sut).toHaveProperty('name', 'Camiseta');
+    expect(sut.price).toBeCloseTo(49.9);
+  });
+});
